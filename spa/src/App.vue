81a57f15e90e9c8447380900c0d785c02fd45659
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-05-10 10:09:17 +0100

    Cards

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="hand">
+    <Card v-for="card in cards" :name="card" :key="card" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Card from "./components/Card.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Card,
+  },
+  data() {
+    return {
+      cards: ["King", "Queen", "Ace", "jack"],
+    };
   },
 });
 </script>
@@ -23,5 +29,10 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.hand {
+  display: flex;
+  flex-direction: row;
 }
 </style>
---
+++ spa/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    name: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.card {
+  width: 2.5in;
+  height: 3.5in;
+  margin-left: 30px;
+  margin-right: 30px;
+  background: coral;
+}
+</style>
